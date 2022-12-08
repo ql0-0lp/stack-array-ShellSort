04c5ea7fe0da825e8eaeebde4332b72307df0116
ql0-0lp <mail@4e1.ru>
2022-12-08 20:57:01 +0300

    Add

---
+++ ShellSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ShellSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>(10); // Сортировка Шелла
+            stack.Push(1);
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(7);
+            stack.Push(2);
+            stack.Push(8);
+            stack.Push(9);
+            stack.Push(0);
+            stack.Push(3);
+            stack.Push(6);
+
+            foreach (var st in stack)
+            {
+                Console.WriteLine(st);
+            }
+
+
+            int len = stack.Size / 2;
+            while (len >= 1)
+            {
+                for (int i = len; i < stack.Size; i++)
+                {
+                    int j = i;
+                    while ((j >= len) && (stack.Get(j - len) > stack.Get(j)))
+                    {
+                        int temp = stack.Get(j);
+                        stack.Set(j, stack.Get(j - len));
+                        stack.Set(j - len, temp);
+                        j -= len;
+                    }
+                }
+
+                len /= 2;
+            }
+
+            Console.WriteLine("---------");
+
+            foreach (var st in stack)
+            {
+                Console.WriteLine(st);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ ShellSort/Stack.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stack<T>
+{
+    private readonly int _Size;
+    private readonly int[] _Array;
+    private int _Top;
+
+    public Stack(int Size)
+    {
+        this._Size = Size;
+        this._Top = 0;
+        this._Array = new int[this._Size];
+    }
+
+    public int Top
+    {
+        get
+        {
+            return this._Top;
+        }
+    }
+
+    public int Size
+    {
+        get
+        {
+            return this._Size;
+        }
+    }
+    public int Count
+    {
+        get
+        {
+            return this._Top;
+        }
+    }
+
+    public bool IsFull()
+    {
+        return this._Top == this._Size;
+    }
+    
+    public bool IsEmpty() // Проверить пустой ли стек
+    {
+        return this._Top == 0;
+    }
+
+    public void Push(int Element)
+    {
+        if (this.IsFull())
+            throw new Exception();
+        this._Array[this._Top++] = Element;
+    }
+    
+    public int Peek() // Вернуть верхний элемент
+    {
+        return this._Array[this._Top - 1];
+    }
+    
+    public int Pop() // Считывание из стека верхнего элемента
+    {
+        return this._Array[--this._Top];
+    }
+
+    public int Get(int index) // Геттер
+    {
+        Stack<string> temp = new Stack<string>(Size - index - 1);
+
+        for (int i = 0; i < Size - index - 1; i++)
+        {
+            temp.Push(this.Pop());
+        }
+        int value = this.Peek();
+        for (int i = 0; i < Size - index - 1; i++)
+        {
+            this.Push(temp.Pop());
+        }
+        return value;
+    }
+
+    public void Set(int index, int value) // Сеттер
+    {
+        Stack<string> temp = new Stack<string>(Size - index - 1);
+
+        for (int i = 0; i < Size - index - 1; i++)
+        {
+            temp.Push(Pop());
+        }
+        this.Pop();
+        this.Push(value);
+        for (int i = 0; i < Size - index - 1; i++)
+        {
+            this.Push(temp.Pop());
+        }
+    }
+
+    public IEnumerator<int> GetEnumerator() // Вывод стека
+    {
+        if (this.IsEmpty())
+            throw new Exception("Стек не заполнен.");
+        for (int i = this._Top; i > 0; i--)
+            yield return _Array[i - 1];
+    }
+}
